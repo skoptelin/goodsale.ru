1a9f6427b45aa02d649106d60605a36bd44343f3
skoptelin <warziher@yandex.ru>
2023-02-20 18:20:06 +0300

    deleted vite script

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Список объявлений</title>
-        <!-- Scripts -->
-        @vite(['resources/js/app.js'])
 
     </head>
     <body>
--- resources/views/ads/updateAd.blade.php
+++ resources/views/ads/updateAd.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Изменить объявление</title>
-        <!-- Scripts -->
-        @vite(['resources/js/app.js', 'resources/sass/app.scss'])
     </head>
     <body>
         <x-app-layout>
